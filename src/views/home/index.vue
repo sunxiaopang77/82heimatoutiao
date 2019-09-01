f294fbe7c648b832359e73badd1c1111a86e5861
孙培峰 <838344306@qq.com>
2019-09-01 09:45:24 +0800

    主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>home</div>
+  <!-- 最外层容器 -->
+  <el-container>
+    <!-- 左侧导航 -->
+    <el-aside></el-aside>
+    <!-- 右侧 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header></el-header>
+      <!-- 主要区域 -->
+      <el-main></el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
